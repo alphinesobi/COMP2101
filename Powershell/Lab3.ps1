c7048cfd51067b5d4d0493081d39618234930647
alphinesobi <alphinesobi@gmail.com>
2021-12-17 11:26:01 -0500

    Create Lab3.ps1

---
+++ Powershell/Lab3.ps1
@@ -0,0 +1,8 @@
+﻿get-ciminstance win32_networkadapterconfiguration | 
+where-object ipenabled -eq 'True' | 
+format-table @{n="ADPTDescription" ; width =25 ; e={$_.description}},
+@{n="Index" ; e={$_.index}},
+@{n="IPAddress" ; width = 25 ; e={$_.ipaddress}},
+@{n="Subnetmask" ; width = 25 ; e={$_.ipsubnet}},
+@{n="DNSDomain" ; width = 25 ; e={$_.DNSDomain}},
+@{n="DNSServer" ; width = 25 ; e={$_.DNSServerSearchOrder}}
